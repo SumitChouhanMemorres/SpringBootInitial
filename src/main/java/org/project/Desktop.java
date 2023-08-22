2141bb5ea47c9d79cc9fe9bb2b71d74a0c29dbb9
Sumit Chauhan <sumit.chouhan@memorres.com>
2023-08-22 18:17:32 +0530

    Changes in Desktop class

--- src/main/java/org/project/Desktop.java
+++ src/main/java/org/project/Desktop.java
@@ -2,6 +2,6 @@ package org.project;
 
 public class Desktop implements Computer{
     public void compile(){
-        System.out.println("Code Compiled in a Desktop");
+        System.out.println("Code Compiled in a Desktop...");
     }
 }
